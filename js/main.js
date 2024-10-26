d02aa5866b37702752114efca5db55f42b2aae89
Nancy Agrawal <nancymycroft@Nancys-MacBook-Air.local>
2024-10-27 01:46:30 +0530

    create and add svg's for logos

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-document.querySelectorAll('.timeline-card').forEach((card) => {
-    card.addEventListener('mouseenter', () => {
-        card.querySelector('.career-details').style.opacity = '1';
-    });
-    card.addEventListener('mouseleave', () => {
-        card.querySelector('.career-details').style.opacity = '0';
+document.querySelectorAll('.fancy-title').forEach(title => {
+    title.addEventListener('click', function () {
+        const details = this.nextElementSibling;
+        const icon = this.querySelector('.expand-icon');
+        
+        if (details) {
+            details.style.visibility = 
+                details.style.visibility === 'visible' ? 'hidden' : 'visible';
+            details.style.opacity = details.style.opacity === '1' ? '0' : '1';
+            icon.style.transform = 
+                details.style.visibility === 'visible' ? 'rotate(180deg)' : 'rotate(0deg)';
+        }
     });
 });
